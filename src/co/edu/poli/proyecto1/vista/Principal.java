96826c29e0cb3600522e22093b62722268990dfb
valero <valero@Col-Soniavalero>
2024-10-22 21:36:39 -0500

    Vista

---
+++ src/co/edu/poli/proyecto1/vista/Principal.java
@@ -0,0 +1,26 @@
+package co.edu.poli.proyecto1.vista;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Principal {
+
+    /**
+     * Default constructor
+     */
+    public Principal() {
+    }
+
+    /**
+     * @param args 
+     * @return
+     */
+    public static void main(String args) {
+        // TODO implement here
+        return null;
+    }
+
+}
